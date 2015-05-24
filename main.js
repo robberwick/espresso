706c79f310367a0dff70f96e90f3ea8a80fca8bc
Rob Berwick <rob.berwick@gmail.com>
2015-05-24 19:55:18 +0100

    First cut at mixed signals

--- main.js
+++ main.js
@@ -47,32 +47,45 @@ RCChannel.prototype.watchHandler = function(e) {
 RCChannelMixer = function() {
     this.inputs = {
         throttle: {
-            value: 0
+            value: 1.5
         },
         rudder: {
-            value: 0
+            value: 1.5
         }
     };
-    this.pin = A1;
+    this.outputPins = {
+        left: C8,
+        right: C9
+    };
 };
 RCChannelMixer.prototype.update = function(channel, value) {
     this.inputs[channel].value = value;
 };
+RCChannelMixer.prototype.getOutput = function() {
+    // Math.min(Math.max(parseInt(number), 1), 20);
+    // RightESC.write(constrain((this.inputs.throttle.value+this.inputs.rudder.value)/2,1000,2000));
+    // LeftESC.write(constrain((-this.inputs.throttle.value+this.inputs.rudder.value)/2+1500,1000,2000));
+    var left = Math.min(Math.max(parseFloat((this.inputs.throttle.value+this.inputs.rudder.value)/2, 10), 1), 2);
+    var right = Math.min(Math.max(parseFloat((-this.inputs.throttle.value+this.inputs.rudder.value)/2+1.5, 10), 1), 2);
+    // console.log(this.inputs.throttle.value, this.inputs.rudder.value);
+    return [
+        [this.outputPins.left, left],
+        [this.outputPins.right, right],
+    ];
 
+};
 var mixer = new RCChannelMixer();
 
 var receiver = new RCReceiver([
     {
         pin: A0,
         callback: function(time){
-            console.log("Pin A0", time.toFixed(2));
             mixer.update('throttle', time);
         }
     },
     {
         pin: C6,
         callback: function(time){
-            console.log("Pin C0:", time);
             mixer.update('rudder', time);
         }
     },
@@ -80,5 +93,7 @@ var receiver = new RCReceiver([
 
 // Set Interval and send calculate mixer value to specified pin
 var interval = setInterval(function () {
-  digitalPulse(mixer.pin, 1, mixer.inputs.throttle.value);
+  mixer.getOutput().map(function(item){
+      digitalPulse(item[0], 1, item[1]);
+  });
 }, 20);
